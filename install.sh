994f9f6d249199f49fd153799f2141ec878be11a
Antonioarf <antonio.awad.roxo@gmail.com>
2023-05-25 17:58:22 -0300

    pqp  quero morrer3

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Build the Docker image
-
+sudo apt install npm
 echo Starting the Docker container...
 curl -sL https://deb.nodesource.com/setup_14.x | sudo -E bash -
 sudo apt install -y nodejs 
